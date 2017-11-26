6d91d8fb615cad431af0b0d7cd10930882cbaa25
rofl0r <retnyg@gmx.net>
2017-11-26 13:30:24 +0000

    update (fix) test.c

--- test.c
+++ test.c
@@ -1,24 +1,24 @@
-#define ENDIANESS_DEBUG
-#include "endianess.h"
+#define ENDIANNESS_DEBUG
+#include "endianness.h"
 
 #include <stdio.h>
 
 int main() {
 
-#if ENDIANESS_LE
+#if ENDIANNESS_LE
 #    define STR "little"
-#elif ENDIANESS_BE
+#elif ENDIANNESS_BE
 #    define STR "big"
 #else
-#    error "unknown endianess"
+#    error "unknown endianness"
 #endif
 
 	puts("this program was compiled for " STR " endian");
 
-	if(ENDIANESS_LE) {
-		puts("yes, the endianess is little!");
-	} else if(ENDIANESS_BE) {
-		puts("yes, the endianess is BIG!");
+	if(ENDIANNESS_LE) {
+		puts("yes, the endianness is little!");
+	} else if(ENDIANNESS_BE) {
+		puts("yes, the endianness is BIG!");
 	} else {
 		puts("this code will never be reached!");
 	}
